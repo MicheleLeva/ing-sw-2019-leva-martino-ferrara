5892b18941aa5f472a533a006d231713c7fde587
stefanomartino97 <stefano97martino@gmail.com>
2019-04-26 10:12:36 +0200

    Add CurrentTurn

---
+++ src/main/java/model/FrenzyTurn.java
@@ -0,0 +1,18 @@
+package model;
+
+public class FrenzyTurn extends Turn {
+
+    public FrenzyTurn(Model model){
+        super(model);
+    }
+
+    @Override
+    public void startTurn(){
+
+    }
+
+    @Override
+    public void notifyTurn(){
+
+    }
+}
---
+++ src/main/java/model/events/playermove/ShowPowerUpMove.java
@@ -0,0 +1,10 @@
+package model.events.playermove;
+
+import model.player_package.PlayerColor;
+import view.View;
+
+public class ShowPowerUpMove extends PlayerMove {
+    public ShowPowerUpMove(View view){
+        super(view);
+    }
+}
